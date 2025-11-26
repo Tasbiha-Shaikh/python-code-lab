9a0331c797692c94e5e1bbcd0198baecc97b3283
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-11-26 15:26:26 +0500

    problem9.py

---
+++ chapter 5/chapter5 ps/problem9.py
@@ -0,0 +1,7 @@
+# can you change a value inside a list which is contained in set s? s = {8,9,"harry",12,[1,2]}
+
+s = {8,9,"harry",12,[1,2]}
+
+
+# No — you cannot change a value inside a list that is inside a set.
+# In fact, you cannot even put a list inside a set.
