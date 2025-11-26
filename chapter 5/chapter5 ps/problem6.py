0ac2d0245667fba9692d8aa6f4d3c8c2e84b049f
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-11-26 15:11:44 +0500

    problem6.py

---
+++ chapter 5/chapter5 ps/problem6.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("enter the friends name :")
+lang = input("enter the languages name :")
+d.update({name : lang})
+
+name = input("enter the friends name :")
+lang = input("enter the languages name :")
+d.update({name : lang})
+
+name = input("enter the friends name :")
+lang = input("enter the languages name :")
+d.update({name : lang})
+
+name = input("enter the friends name :")
+lang = input("enter the languages name :")
+d.update({name : lang})
+
+print(d)
