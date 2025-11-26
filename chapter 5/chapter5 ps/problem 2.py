683c5f86b159b129904959e8d0b4fc6777489c6a
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-11-26 14:15:51 +0500

    problem 2.py

---
+++ chapter 5/chapter5 ps/problem 2.py
@@ -0,0 +1,38 @@
+s = set()
+
+n = input("enter the number 1:")
+s.add(int(n))
+
+n = input("enter the number 2:")
+s.add(int(n))
+
+n = input("enter the number 3:")
+s.add(int(n))
+
+n = input("enter the number 4:")
+s.add(int(n))
+
+n = input("enter the number 5:")
+s.add(int(n))
+
+n = input("enter the number 6:")
+s.add(int(n))
+
+n = input("enter the number 7:")
+s.add(int(n))
+
+n = input("enter the number 8:")
+s.add(int(n))
+
+print(s)
+
+
+
+
+
+
+
+
+
+
+
