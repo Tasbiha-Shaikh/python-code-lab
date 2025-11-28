266b6aa30355808b6f4555e407a9d15ba2f69a2a
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-11-28 13:56:37 +0500

    problem 1.py

---
+++ chapter 6/chapter6 ps/problem 1.py
@@ -0,0 +1,16 @@
+a1 = int(input("enter the no 1 :"))
+a2 = int(input("enter the no 2 :"))
+a3 = int(input("enter the no 3 :"))
+a4 = int(input("enter the no 4 :"))
+
+if(a1>a2 and a1>a3 and a1>a4):
+    print("greatest no is a1",a1)
+
+elif(a2>a1 and a2>a3 and a2>a4):
+    print("greatest no is a2",a2)
+
+elif(a3>a1 and a3>a2 and a3>a4):
+    print("greatest no is a3",a3)
+
+elif(a4>a1 and a4>a2 and a4>a3):
+    print("greatest the no is a4",a4)
