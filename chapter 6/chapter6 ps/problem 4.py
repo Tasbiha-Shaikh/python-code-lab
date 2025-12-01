52fc2154ab398ed25364c089d8a2bf4a94a98230
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-12-01 21:35:02 +0500

    problem 4.py

---
+++ chapter 6/chapter6 ps/problem 4.py
@@ -0,0 +1,7 @@
+username = input("enter username:")
+
+if(len(username)<10):
+    print("your username contains less than 10 characters")
+
+else:
+    print("your username contains more than 10 characters")
