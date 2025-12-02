172144ff03ec56b0ef22a73bfe28ecce32fe22c8
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-12-02 19:12:09 +0500

    statements.py

---
+++ chapter 7/statements.py
@@ -0,0 +1,25 @@
+# #break statement
+# for i in range(80):
+#      if (i==67):
+#          break # exist the loop right now
+#      print(i)
+   
+
+# #continue statement
+# for i in range(80):
+#      if (i==67):  
+#          continue # skip this iteration
+#      print(i)
+
+# for i in range(4):
+#      print("printing")
+#      if(i==2):
+#           continue
+#      print(i)
+
+
+#pass statement
+
+for item in range(890):
+    pass
+
