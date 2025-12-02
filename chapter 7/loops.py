92bd0e844905e3f3fff209f7ed98825b3b2f8b92
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-12-02 18:34:34 +0500

    loops.py

---
+++ chapter 7/loops.py
@@ -0,0 +1,17 @@
+for i in range(1,100):
+    print(i)
+
+#write a program to print 1 to 50 using while loop
+i = 0
+while(i<51):
+    print(i)
+    i+=1  # or i = i+1
+
+
+# write a program to print the content of list using while loop
+l = [1,"harry","rohan",False,"orange","red"]
+
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i = i+1
