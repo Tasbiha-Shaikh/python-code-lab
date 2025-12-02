d953c167e32d7e5b9d741e6ddb74630315b08948
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-12-02 18:53:03 +0500

    for.py

---
+++ chapter 7/for .py
@@ -0,0 +1,18 @@
+# s = "harry"
+# for i in s:
+#     print(i)
+
+# t = (1,6,456,234)
+# for i in t:
+#     print(i)
+
+# for i in  range (0,7):
+#     print(i)
+
+# for loop with else
+l = [1,2,5,3]
+for item in l:
+    print (item)
+
+else:
+    print("done")
