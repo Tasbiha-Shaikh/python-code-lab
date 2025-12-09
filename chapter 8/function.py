f5a220818ba8c5bd711710ed4b295976798f75e8
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-12-09 14:55:28 +0500

    function.py

---
+++ chapter 8/function.py
@@ -0,0 +1,26 @@
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+
+    average = (a + b + c) / 3
+    print("Average:", average)
+
+avg()
+
+
+def func1():
+    print("hello")
+
+
+func1()
+
+
+#  quiz write a progrom to greet a user by good day uusing function
+
+def greet():
+    name = input("Enter your name: ")
+    print("Good Day", name)
+
+greet()
+
