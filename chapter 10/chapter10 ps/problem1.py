29daeaa10aea8e20796ce607acf73891ac7b14a5
Tasbiha-Shaikh <tasbihairfan730@gmail.com>
2025-12-21 12:17:34 +0500

    problem1.py

---
+++ chapter 10/chapter10 ps/problem1.py
@@ -0,0 +1,10 @@
+class Programmer:
+    company = "microsoft"
+
+    def __init__(self, name, salary, pin):
+        self.name = name
+        self.salary = salary
+        self.pin = pin
+
+p = Programmer("rohan", 1200000, 234501)
+print(p.name, p.salary, p.pin, p.company)
